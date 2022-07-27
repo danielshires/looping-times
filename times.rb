d93e59806049e666a6cce1b46685ebfd43754cb3
danielshires <hello@danielshires.com>
2022-07-27 14:01:32 +1000

    Assignment complete

--- times.rb
+++ times.rb
@@ -1,7 +1,23 @@
 def using_times
 	#your code here
+	7.times do 
+		puts "Wingardium Leviosa"
+	end
 end
 
+using_times
 
+# jewels = 100
 
+# # 3.times do
+# # 	puts "hiding 10 jewels"
+# # 	jewels = jewels - 10
+# # end
 
+# # puts "We have #{jewels} jewels still to hide!"
+
+# 3.times do 
+# 	puts "Hiding 10 stolen jewels"
+# 	jewels = jewels - 10
+# 	puts "Now there are only #{jewels} jewels left to hide!"
+# end
